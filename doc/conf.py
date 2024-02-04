a84516a1f2f12e808d343e32d3c2a3b0c0c553ac
Puff Machine <puff@127.0.0.1>
2024-02-04 09:54:45 +0000

    Switch from legacy sphinxcontrib.napoleon to sphinx.ext.napoleon

--- doc/conf.py
+++ doc/conf.py
@@ -34,7 +34,7 @@
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
-    'sphinxcontrib.napoleon']
+    'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
